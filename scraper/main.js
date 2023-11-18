376077a0fb8596f8e660c108fe8a6e6ffa097551
Az-Heda <tecno19dragon@gmail.com>
2023-11-18 14:35:19 +0100

    -

---
+++ scraper/main.js
@@ -0,0 +1,17 @@
+
+
+const urls = {
+	characters: 'https://genshin.honeyhunterworld.com/fam_chars/?lang=EN',
+	artifacts: '',
+	weapons: {
+		sword: '',
+		bow: '',
+		polyarm: '',
+		catalyst: '',
+		claymore: ''
+	},
+	enemies: {
+		boss: '',
+		normal: '',
+	}
+}
